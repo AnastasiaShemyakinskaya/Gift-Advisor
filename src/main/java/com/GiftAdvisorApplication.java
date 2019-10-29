5eceffe05308d0ef754ec6d1b25ee8e1fb75401c
Anastasia Shemyakinskaya <36507473+AnastasiaShemyakinskaya@users.noreply.github.com>
2019-10-29 23:42:10 +0100

    Added gradle project

---
+++ src/main/java/com/GiftAdvisorApplication.java
@@ -0,0 +1,16 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class GiftAdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftAdvisorApplication.class, args);
+	}
+
+}
